712d80dff9080b08b097e9003031cffc8318677a
Thomas Rudolph <me@holloway-web.de>
2016-11-11 02:04:26 +0100

    when nothing to build, builders repair roads

--- default/role.builder.js
+++ default/role.builder.js
@@ -15,10 +15,24 @@ module.exports = {
                 if (ERR_NOT_IN_RANGE == creep.build(targets[0])) {
                     creep.moveTo(targets[0]);
                 }
+            } else {
+                targets = creep.room.find(FIND_MY_STRUCTURES, {
+                    /**
+                     * @param {Structure} structure
+                     */
+                    filter: function (structure) {
+                        return STRUCTURE_ROAD == structure.structureType && structure.hits < structure.hitsMax;
+                    }
+                });
+                if (targets.length) {
+                    if (ERR_NOT_IN_RANGE == creep.repair(targets[0])) {
+                        creep.moveTo(targets[0]);
+                    }
+                }
             }
         } else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (ERR_NOT_IN_RANGE == creep.harvest(sources[0])) {
                 creep.moveTo(sources[0]);
             }
         }

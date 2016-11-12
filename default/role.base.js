5f15d4438017f1aaa0da385aff3d64fd9b25bbc0
Thomas Rudolph <me@holloway-web.de>
2016-11-12 11:56:55 +0100

    search next source if current is full of creeps

--- default/role.base.js
+++ default/role.base.js
@@ -1,4 +1,35 @@
-module.exports = {
+var roleBase = {
+    /**
+     * @param {Source} source
+     * @param {Creep} creep
+     * @return {int}
+     */
+    getEnergySourceCost: function(source, creep) {
+        var placesCnt = 0;
+        var creepsCnt = 0;
+        for (var y = -1; y <= 1; y++) {
+            for (var x = -1; x <= 1; x++) {
+                if (0 == x && 0 == y) {
+                    continue;
+                }
+
+                var objects = creep.room.lookAt(source.pos.x + x, source.pos.y + y);
+                for (var o = 0; o < objects.length; o++) {
+                    if (LOOK_TERRAIN == objects[o].type &&
+                        ('swamp' == objects[o].terrain || 'plain' == objects[o].terrain)
+                    ) {
+                        placesCnt++;
+                    }
+                    if (LOOK_CREEPS == objects[o].type) {
+                        creepsCnt++;
+                    }
+                }
+            }
+        }
+
+        return creepsCnt - placesCnt;
+    },
+
     /**
      * @param {Creep} creep
      */
@@ -8,28 +39,7 @@ module.exports = {
         var lowestCostSource = 0;
         var lowestCost = null;
         for (var s = 0; s < sources.length; s++) {
-            var placesCnt = 0;
-            var creepsCnt = 0;
-            for (var y = -1; y <= 1; y++) {
-                for (var x = -1; x <= 1; x++) {
-                    if (0 == x && 0 == y) {
-                        continue;
-                    }
-
-                    var objects = creep.room.lookAt(sources[s].pos.x + x, sources[s].pos.y + y);
-                    for (var o = 0; o < objects.length; o++) {
-                        if (LOOK_TERRAIN == objects[o].type &&
-                            ('swamp' == objects[o].terrain || 'plain' == objects[o].terrain)
-                        ) {
-                            placesCnt++;
-                        }
-                        if (LOOK_CREEPS == objects[o].type) {
-                            creepsCnt++;
-                        }
-                    }
-                }
-            }
-            var cost = creepsCnt - placesCnt;
+            var cost = roleBase.getEnergySourceCost(sources[s], creep);
             if (null == lowestCost || lowestCost > cost) {
                 lowestCost = cost;
                 lowestCostSource = s;
@@ -44,8 +54,18 @@ module.exports = {
      */
     harvestSourceInMemory: function (creep) {
         var source = Game.getObjectById(creep.memory.harvestingSource);
-        if (ERR_NOT_IN_RANGE == creep.harvest(source)) {
+        var success = creep.harvest(source);
+        if (creep.memory.moving && OK == success) {
+            if (0 == roleBase.getEnergySourceCost(source, creep)) {
+                roleBase.setBestEnergySource(creep);
+            }
+            creep.memory.moving = false;
+        }
+        if (ERR_NOT_IN_RANGE == success) {
             creep.moveTo(source);
+            creep.memory.moving = true;
         }
     }
 };
+
+module.exports = roleBase;

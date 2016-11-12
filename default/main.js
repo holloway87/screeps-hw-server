e30438025f2b185ea4dcd5d6278953983591c483
Thomas Rudolph <me@holloway-web.de>
2016-11-12 17:35:17 +0100

    generate bigger creeps if energy is available

--- default/main.js
+++ default/main.js
@@ -21,8 +21,16 @@ function updateRoom(room) {
         function (creep) {return globals.ROLE_WORKER == creep.memory.role;}
     );
 
-    if (globals.maxWorker > workers.length && 200 <= room.energyAvailable) {
-        Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], null, {role: globals.ROLE_WORKER});
+    if (1050 <= room.energyCapacityAvailable) {
+        if (globals.maxWorker > workers.length && 1050 <= room.energyAvailable) {
+            Game.spawns['Spawn1'].createCreep(
+                [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+                null, {role: globals.ROLE_WORKER});
+        }
+    } else {
+        if (globals.maxWorker > workers.length && 200 <= room.energyAvailable) {
+            Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], null, {role: globals.ROLE_WORKER});
+        }
     }
 
     _.forEach(workers,

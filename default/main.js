73867739dc15b87fddbcc894315b51a947fd8d0d
Thomas Rudolph <me@holloway-web.de>
2017-10-21 15:22:57 +0200

    removed upgrader, harvester does all the work and reproduces

--- default/main.js
+++ default/main.js
@@ -1,19 +1,47 @@
 'use strict';
 
 let roleHarvester = require('role.harvester');
-let roleUpgrader = require('role.upgrader');
 
-module.exports.loop = function () {
-    for (let name in Game.creeps) {
-        if (!Game.creeps.hasOwnProperty(name)) {
+/**
+ * Update one room.
+ *
+ * @param {Room} room
+ */
+function updateRoom(room) {
+    let harvesters = room.find(FIND_MY_CREEPS,
+        /** @param {Creep} creep */
+        function (creep) {return 'harvester' === creep.memory.role;}
+    );
+
+    let harvestersCnt = 0;
+    for (let name in harvesters) {
+        if (!harvesters.hasOwnProperty(name)) {
             continue;
         }
 
-        let creep = Game.creeps[name];
+        let creep = harvesters[name];
         if ('harvester' === creep.memory.role) {
             roleHarvester.run(creep);
-        } else if ('upgrader' === creep.memory.role) {
-            roleUpgrader.run(creep);
+            harvestersCnt++;
         }
+    }
+
+    if (roleHarvester.maxCreeps > harvestersCnt) {
+        let spawns = Game.rooms['W3N8'].find(FIND_MY_SPAWNS);
+        for (let i = 0; i < spawns.length; i++) {
+            if (spawns[i].energy >= roleHarvester.creepEnergyNeeded) {
+                roleHarvester.create(spawns[i]);
+            }
+        }
+    }
+}
+
+module.exports.loop = function () {
+    for (let name in Game.rooms) {
+        if (!Game.rooms.hasOwnProperty(name)) {
+            continue;
+        }
+
+        updateRoom(Game.rooms[name]);
     }
 };
--- default/role.harvester.js
+++ default/role.harvester.js
@@ -1,23 +1,148 @@
 'use strict';
 
-let roleHarvester = {
+/**
+ * Harvester role.
+ *
+ * Harvests energy and distributes it to spawns and room controllers.
+ *
+ * @returns {{run: run}}
+ * @constructor
+ */
+let RoleHarvester = function () {
+    const STATE_HARVEST = 0;
+    const STATE_TRANSFER = 1;
+    const STATE_UPGRADE_CONTROLLER = 2;
+
+    const MAX_CREEPS = 3;
+
+    let creepIdx = 0;
+
+    return {
+        'create': create,
+        'creepEnergyNeeded': 200,
+        'maxCreeps': MAX_CREEPS,
+        'run': run
+    };
+
+    /**
+     * Creates a new harvester.
+     *
+     * @param {StructureSpawn} spawn
+     */
+    function create(spawn) {
+        spawn.spawnCreep([CARRY, MOVE, WORK], 'harvester' + creepIdx, {'memory': {'role': 'harvester'}});
+        creepIdx++;
+        if (MAX_CREEPS <= creepIdx) {
+            creepIdx = 0;
+        }
+    }
+
+    /**
+     * Harvest resources.
+     *
+     * When finished, upgrade controller.
+     *
+     * @param {Creep} creep
+     */
+    function harvest(creep) {
+        if (creep.carry.energy < creep.carryCapacity) {
+            let source = Game.getObjectById(creep.memory.sourceId);
+            if (ERR_NOT_IN_RANGE === creep.harvest(source)) {
+                creep.moveTo(source);
+            }
+        } else {
+            setToTransferEnergy(creep);
+        }
+    }
+
     /**
      * Harvest resources and put them into the spawn.
      *
      * @param {Creep} creep
      */
-    run: function (creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
-            let sources = creep.room.find(FIND_SOURCES);
-            if (ERR_NOT_IN_RANGE === creep.harvest(sources[0])) {
-                creep.moveTo(sources[0]);
+    function run(creep) {
+        if (STATE_HARVEST === creep.memory.state) {
+            harvest(creep);
+        } else if (STATE_TRANSFER === creep.memory.state) {
+            transfer(creep);
+        } else if (STATE_UPGRADE_CONTROLLER === creep.memory.state) {
+            upgradeController(creep);
+        } else {
+            // new guy
+            setToHarvest(creep);
+        }
+    }
+
+    /**
+     * Send harvest to next available energy source.
+     *
+     * @param {Creep} creep
+     */
+    function setToHarvest(creep) {
+        let sources = creep.room.find(FIND_SOURCES);
+        creep.memory.sourceId = sources[0].id;
+        creep.memory.state = STATE_HARVEST;
+    }
+
+    /**
+     * Decides where to transfer the energy to.
+     *
+     * If a spawn doesn't have full energy, transfer it there, otherwise upgrade the controller.
+     *
+     * @param creep
+     */
+    function setToTransferEnergy(creep) {
+        let spawns = creep.room.find(FIND_MY_SPAWNS);
+        for (let i = 0; i < spawns.length; i++) {
+            if (spawns[i].energy < spawns[i].energyCapacity) {
+                creep.memory.state = STATE_TRANSFER;
+                creep.memory.sourceId = spawns[i].id;
+
+                return;
+            }
+        }
+
+        creep.memory.state = STATE_UPGRADE_CONTROLLER;
+    }
+
+    /**
+     * Transfer energy to a spawn.
+     *
+     * @param {Creep} creep
+     */
+    function transfer(creep) {
+        if (0 < creep.carry.energy) {
+            let spawn = Game.getObjectById(creep.memory.sourceId);
+
+            // if the spawn is full suddenly set target again
+            if (spawn.energy === spawn.energyCapacity) {
+                setToTransferEnergy(creep);
+
+                return;
+            }
+
+            if (ERR_NOT_IN_RANGE === creep.transfer(spawn, RESOURCE_ENERGY)) {
+                creep.moveTo(spawn);
             }
         } else {
-            if (ERR_NOT_IN_RANGE === creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY)) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            setToHarvest(creep);
+        }
+    }
+
+    /**
+     * Upgrade the room controller.
+     *
+     * @param {Creep} creep
+     */
+    function upgradeController(creep) {
+        if (0 < creep.carry.energy) {
+            if (ERR_NOT_IN_RANGE === creep.upgradeController(creep.room.controller)) {
+                creep.moveTo(creep.room.controller);
             }
+        } else {
+            setToHarvest(creep);
         }
     }
 };
 
-module.exports = roleHarvester;
+module.exports = new RoleHarvester();
